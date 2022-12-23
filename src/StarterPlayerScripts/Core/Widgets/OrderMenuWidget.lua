4756e2c0713e2e0250e30925595b5c362ef8392d
SPOOKEXE <91944990+SPOOKEXE@users.noreply.github.com>
2022-12-23 14:34:19 +0930

    base-widget

---
+++ src/StarterPlayerScripts/Core/Widgets/OrderMenuWidget.lua
@@ -0,0 +1,50 @@
+
+local Players = game:GetService('Players')
+local LocalPlayer = Players.LocalPlayer
+local LocalAssets = LocalPlayer:WaitForChild('PlayerScripts'):WaitForChild('Assets')
+local LocalModules = require(LocalPlayer:WaitForChild('PlayerScripts'):WaitForChild('Modules'))
+
+local Interface = LocalPlayer:WaitForChild('PlayerGui'):WaitForChild('Interface')
+local MenuOrderFrame = Interface:WaitForChild('MenuOrderFrame')
+
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+local ReplicatedModules = require(ReplicatedStorage:WaitForChild('Modules'))
+
+local SystemsContainer = {}
+
+-- // Module // --
+local Module = { Open = false }
+Module.WidgetMaid = ReplicatedModules.Classes.Maid.New()
+
+-- function Module:UpdateWidget()
+-- 	if not Module.Open then
+-- 		return
+-- 	end
+-- 	-- update
+-- end
+
+function Module:OpenWidget()
+	if Module.Open then
+		return
+	end
+	Module.Open = true
+	-- on open
+	MenuOrderFrame.Visible = true
+	-- Module:UpdateWidget()
+end
+
+function Module:CloseWidget()
+	if not Module.Open then
+		return
+	end
+	Module.Open = false
+	MenuOrderFrame.Visible = false
+	Module.WidgetMaid:Cleanup()
+	-- on close
+end
+
+function Module:Init( otherSystems )
+	SystemsContainer = otherSystems
+end
+
+return Module
